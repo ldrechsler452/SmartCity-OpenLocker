82e6a7ce58ecab174f66169699594bb7f5ff85be
lukas-brinkmeier <l.brinkmeier@galawork.de>
2024-10-27 00:38:55 +0200

    added responses

--- open-locker-laravel/app/Http/Controllers/ContentController.php
+++ open-locker-laravel/app/Http/Controllers/ContentController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Content;
 use App\Models\User;
 use App\Services\ScriptService;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ContentController extends Controller
 {
-    public function take(Content $content): void
+    public function take(Content $content): Response
     {
         /** @var User $currentUser */
         $currentUser = auth()->user();
@@ -18,9 +20,13 @@ class ContentController extends Controller
             ->save();
 
         ScriptService::openLocker($content->getLocker());
+
+        return Inertia::render('Content/Taken', [
+            'content' => $content,
+        ]);
     }
 
-    public function return(Content $content): void
+    public function return(Content $content): Response
     {
         $content
             ->setUser(null)
@@ -28,5 +34,9 @@ class ContentController extends Controller
             ->save();
 
         ScriptService::openLocker($content->getLocker());
+
+        return Inertia::render('Content/Returned', [
+            'content' => $content,
+        ]);
     }
 }
