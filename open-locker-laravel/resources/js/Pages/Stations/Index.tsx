e08840c9061ec63fa3d39bee42c8299acab6e9ff
Alexander Baltruschat <alexander.baltruschat@mailbox.org>
2024-10-26 19:33:45 +0200

    add lockers link

--- open-locker-laravel/resources/js/Pages/Stations/Index.tsx
+++ open-locker-laravel/resources/js/Pages/Stations/Index.tsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Station } from '@/types';
 import { Head } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
 
 export default function Stations({ stations }: { stations: Station[] }) {
     return (
@@ -20,9 +21,9 @@ export default function Stations({ stations }: { stations: Station[] }) {
                             <div className='flex flex-col gap-4'>
                                 {stations.map((station) => (
                                     <div key={station.id} className="w-100 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                        <a href="#">
+                                        <Link href={`/stations/${station.id}/lockers`}>
                                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{station.name}</h5>
-                                        </a>
+                                        </Link>
                                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                                             Lockers: {station.lockers?.length ?? 0}
                                         </p>
