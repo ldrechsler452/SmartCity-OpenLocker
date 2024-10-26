44134f0ca38a963122fd976b4be3f0162994c269
Simon Engelhardt <smn@nglhrdt.de>
2024-10-26 23:57:03 +0200

    lockerItem details

--- open-locker-laravel/resources/js/Pages/Dashboard.tsx
+++ open-locker-laravel/resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,8 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { TLockerItem } from '@/types';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard() {
+export default function Dashboard({content}:{content: TLockerItem}) {
     return (
         <AuthenticatedLayout
             header={
@@ -16,7 +17,7 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            Keine Artikel ausgeliehen.
+                            <pre>{content && JSON.stringify(content, null, 2)}</pre>
                         </div>
                     </div>
                 </div>
