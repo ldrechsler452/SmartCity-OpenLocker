cd209e9fad9a6d68ffb2ccce58ab8ad6a419eaf5
lukas-brinkmeier <l.brinkmeier@galawork.de>
2024-10-26 17:05:31 +0200

    prepared locker open and close

--- open-locker-laravel/app/Http/Controllers/LockerController.php
+++ open-locker-laravel/app/Http/Controllers/LockerController.php
@@ -5,13 +5,31 @@ namespace App\Http\Controllers;
 use App\Models\Locker;
 use App\Models\Station;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class LockerController extends Controller
 {
-    public function index(Station $station)
+    public function index(Station $station): Response
     {
         return Inertia::render('Locker/LockerIndex', [
             'lockers' => $station->getLockers(),
         ]);
+    }
+
+    public function open(Locker $locker): void
+    {
+        // TODO: Tell API to open the door
+
+        $locker
+            ->open()
+            ->setLastOpenedAt(now())
+            ->save();
+    }
+
+    public function close(Locker $locker): void
+    {
+        $locker
+            ->close()
+            ->save();
     }
 }
