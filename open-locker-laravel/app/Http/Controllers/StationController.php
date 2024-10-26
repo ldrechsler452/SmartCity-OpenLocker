200225b62ea56f8f70536200ffc7f63611502175
lukas-brinkmeier <l.brinkmeier@galawork.de>
2024-10-27 00:22:26 +0200

    updated station index endpoint

--- open-locker-laravel/app/Http/Controllers/StationController.php
+++ open-locker-laravel/app/Http/Controllers/StationController.php
@@ -12,30 +12,31 @@ class StationController extends Controller
 {
     public function index(): Response
     {
-        $stations = Station::query();
-
-        // If a specific Content is requested, filter for stations that have the content.
-        $searchedContentName = request()->input('searched_content_name');
-        if (null !== $searchedContentName)
-        {
-            // TODO: In the future, optimize this query with a join.
-            $contentsFound = Content::query()
-                ->whereLike('name', $searchedContentName)
-                ->pluck('id')
-                ->toArray();
-
-            $stationsWithSearchedContents = Locker::query()
-                ->whereIn('content_id', $contentsFound)
-                ->pluck('station_id')
-                ->toArray();
-
-            $stations->whereIn('id', $stationsWithSearchedContents);
-        }
+        // TODO: searched content name logic can be removed when it's handled in the front end instead.
+//        $stations = Station::query();
+//
+//        // If a specific Content is requested, filter for stations that have the content.
+//        $searchedContentName = request()->input('searched_content_name');
+//        if (null !== $searchedContentName)
+//        {
+//            $contentsFound = Content::query()
+//                ->whereLike('name', $searchedContentName)
+//                ->pluck('id')
+//                ->toArray();
+//
+//            $stationsWithSearchedContents = Locker::query()
+//                ->whereIn('content_id', $contentsFound)
+//                ->pluck('station_id')
+//                ->toArray();
+//
+//            $stations->whereIn('id', $stationsWithSearchedContents);
+//        }
 
         return Inertia::render('Stations/Index', [
-            'stations' => $stations->get()->load('lockers'),
-            'contentNames' => Content::all()->pluck('name')->toArray(),
-            'searchedContentName' => $searchedContentName,
+//            'stations' => $stations->get()->load('lockers'),
+            'stations' => Station::all()->load('lockers', 'lockers.content'),
+//            'contentNames' => Content::all()->pluck('name')->toArray(),
+//            'searchedContentName' => $searchedContentName,
         ]);
     }
 }
