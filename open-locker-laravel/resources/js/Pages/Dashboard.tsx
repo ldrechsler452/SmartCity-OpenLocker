87c72a8a2e02058669329d439783848327851f73
Simon Engelhardt <smn@nglhrdt.de>
2024-10-27 01:09:31 +0200

    add filter

--- open-locker-laravel/resources/js/Pages/Dashboard.tsx
+++ open-locker-laravel/resources/js/Pages/Dashboard.tsx
@@ -1,23 +1,26 @@
+import LockerItem from '@/Components/LockerItem';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { TLockerItem } from '@/types';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard({content}:{content: TLockerItem}) {
+export default function Dashboard({ content }: { content: TLockerItem[] }) {
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Ausleihe
+                    Geliehene Artikel
                 </h2>
             }
         >
             <Head title="Ausleihe" />
 
-            <div className="py-0 sm:py-12"> 
+            <div className="py-0 sm:py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <pre>{content && JSON.stringify(content, null, 2)}</pre>
+                            <div className="flex flex-col gap-4">
+                                {content.map((item) => <LockerItem key={item.id} lockerItem={item} />)}
+                            </div>
                         </div>
                     </div>
                 </div>
--- open-locker-laravel/resources/js/Pages/Stations/Index.tsx
+++ open-locker-laravel/resources/js/Pages/Stations/Index.tsx
@@ -1,9 +1,27 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Station } from '@/types';
-import { Head } from '@inertiajs/react';
-import { Link } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
+import { useState } from 'react';
+import Select, { ActionMeta, Options, SingleValue } from 'react-select';
 
 export default function Stations({ stations }: { stations: Station[] }) {
+    const [filteredStations, setFilteredStations] = useState(stations);
+    const [selectedOption, setSelectedOption] = useState<SingleValue<{ value: string; label: string }>>(null);
+
+    const uniqueContentNames = Array.from(new Set(stations.flatMap(station =>
+        station.lockers.flatMap(locker => locker.content.name)
+    )));
+    const options = uniqueContentNames.map((name: string) => ({ value: name, label: name }));
+
+    function handleSelectionChange(newValue: SingleValue<{ value: string; label: string; }>, actionMeta: ActionMeta<{ value: string; label: string; }>): void {
+        setSelectedOption(newValue);
+        if (newValue) {
+            setFilteredStations(stations.filter(station => station.lockers.some(locker => locker.content.name === newValue.value)));
+        } else {
+            setFilteredStations(stations);
+        }
+    }
+
     return (
         <AuthenticatedLayout
             header={
@@ -19,7 +37,20 @@ export default function Stations({ stations }: { stations: Station[] }) {
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className='flex flex-col gap-4'>
-                                {stations.map((station) => (
+                                <div className="flex gap-4">
+                                    <h3 className="text-xl font-bold">Filter</h3>
+                                    <Select
+                                        className="flex-grow"
+                                        options={options}
+                                        getOptionLabel={(option) => option.label}
+                                        getOptionValue={(option) => option.value}
+                                        isClearable
+                                        isSearchable
+                                        defaultValue={selectedOption}
+                                        onChange={handleSelectionChange}
+                                    />
+                                </div>
+                                {filteredStations.map((station) => (
                                     <div key={station.id} className="w-100 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                                         <Link href={`/stations/${station.id}/lockers`}>
                                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{station.name}</h5>
