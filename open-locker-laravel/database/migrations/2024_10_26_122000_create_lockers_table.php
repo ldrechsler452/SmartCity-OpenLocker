478bb4958d85c3400b4a57c63a72c54a53cc05aa
lukas-brinkmeier <l.brinkmeier@galawork.de>
2024-10-26 16:13:58 +0200

    added is_open field

--- open-locker-laravel/database/migrations/2024_10_26_122000_create_lockers_table.php
+++ open-locker-laravel/database/migrations/2024_10_26_122000_create_lockers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('lockers', function (Blueprint $table) {
             $table->id();
             $table->string('designation')->unique();
+            $table->boolean('is_open')->default(false);
             $table->timestamp('last_opened_at')->nullable();
             $table->timestamps();
         });
