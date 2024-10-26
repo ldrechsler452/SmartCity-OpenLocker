340894d5660d76ca638cf69350e0b37d4666b2f1
lukas-brinkmeier <l.brinkmeier@galawork.de>
2024-10-26 23:46:15 +0200

    updated station index endpoint

--- open-locker-laravel/app/Http/Controllers/StationController.php
+++ open-locker-laravel/app/Http/Controllers/StationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Content;
+use App\Models\Locker;
 use App\Models\Station;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -10,8 +12,30 @@ class StationController extends Controller
 {
     public function index(): Response
     {
+        $stations = Station::query();
+
+        // If a specific Content is requested, filter for stations that have the content.
+        $searchedContentName = request()->input('searched_content_name');
+        if (null !== $searchedContentName)
+        {
+            // TODO: In the future, optimize this query with a join.
+            $contentsFound = Content::query()
+                ->whereLike('name', $searchedContentName)
+                ->pluck('id')
+                ->toArray();
+
+            $stationsWithSearchedContents = Locker::query()
+                ->whereIn('content_id', $contentsFound)
+                ->pluck('station_id')
+                ->toArray();
+
+            $stations->whereIn('id', $stationsWithSearchedContents);
+        }
+
         return Inertia::render('Stations/Index', [
-            'stations' => Station::all()->load('lockers'),
+            'stations' => $stations->get()->load('lockers'),
+            'contentNames' => Content::all()->pluck('name')->toArray(),
+            'searchedContentName' => $searchedContentName,
         ]);
     }
 }
