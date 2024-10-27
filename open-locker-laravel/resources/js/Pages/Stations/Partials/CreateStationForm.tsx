8f97bbeaaf3e5c4bc655922a543b9b141a1808ac
lukas-brinkmeier <l.brinkmeier@galawork.de>
2024-10-27 02:27:39 +0200

    fix

--- open-locker-laravel/resources/js/Pages/Stations/Partials/CreateStationForm.tsx
+++ open-locker-laravel/resources/js/Pages/Stations/Partials/CreateStationForm.tsx
@@ -3,11 +3,10 @@ import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import { Transition } from '@headlessui/react';
-import { Link, useForm, usePage } from '@inertiajs/react';
+import { useForm } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
 
 export default function UpdateProfileInformation({
-    status,
     className = '',
 }: {
     status?: string;
